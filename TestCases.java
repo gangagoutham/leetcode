cedfae721beb48f36269177f2cefd58415e1fe62
gangagoutham <gangamani456@gmail.com>
2024-12-03 18:47:23 +0530

    Add files via upload

---
+++ TestCases.java
@@ -0,0 +1,138 @@
+package demo;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import java.util.logging.Level;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class TestCases {
+    ChromeDriver driver;
+    public TestCases()
+    {
+        System.out.println("Constructor: TestCases");
+
+        WebDriverManager.chromedriver().timeout(30).setup();
+        ChromeOptions options = new ChromeOptions();
+        LoggingPreferences logs = new LoggingPreferences();
+
+        // Set log level and type
+        logs.enable(LogType.BROWSER, Level.ALL);
+        logs.enable(LogType.DRIVER, Level.ALL);
+        options.setCapability("goog:loggingPrefs", logs);
+
+        // Set path for log file
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "chromedriver.log");
+
+        driver = new ChromeDriver(options);
+
+        // Set browser to maximize and wait
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+
+    public void endTest()
+    {
+        System.out.println("End Test: TestCases");
+        driver.close();
+        driver.quit();
+
+    }
+
+   
+    public  void testCase01(){
+        System.out.println("Start Test case: testCase01");
+        driver.get(" https://leetcode.com/");
+        String currentURL = driver.getCurrentUrl();
+        if(currentURL.contains("leetcode")){
+            System.out.println(" The URL contains leetcode");
+        }else{
+            System.out.println(" The URL does not contain leetcode");
+        }
+        System.out.println("end Test case: testCase01");
+    }
+    
+   
+    public  boolean testCase02(){
+        System.out.println("Start Test case: testCase02");
+      driver.findElement(By.xpath(" //p[contains(text(),'Questions')]")).click();
+
+       String currentUrl=driver.getCurrentUrl();
+       if(currentUrl.contains("problemset")){
+        System.out.println("url contains problemset");
+       }
+        try {
+            Thread.sleep(5000); // Wait for the page to load
+        } catch (InterruptedException e) {
+            System.out.println("Sleep interrupted: " + e.getMessage());
+        }
+      List<WebElement> list=driver.findElements(By.xpath(" //a[@class='h-5 hover:text-blue-s dark:hover:text-dark-blue-s']"));
+      int inputCount=6;
+      for(int i=0;i<inputCount;i++){
+        System.out.println(list.get(i).getText());
+      }
+
+        System.out.println("end Test case: testCase02");
+     
+      //a[text()='Two Sum']
+
+      //span[text()='Submissions']
+      //a[text()='Register or Sign In']
+      
+        return true;
+    }
+
+   
+    public void testCase03() {
+        System.out.println("Start Test case: testCase03");
+     try{  
+    WebElement firstProblem = driver.findElement(By.xpath("//a[text()='Two Sum']"));
+    
+    firstProblem.click();
+   
+    String currentUrl = driver.getCurrentUrl();
+ 
+    if(currentUrl.contains("two-sum")){
+        System.out.println("URL verification passed: 'two-sum' is in the URL");
+    }else{
+        System.out.println("URL verification failed: 'two-sum' is in the URL");
+    }
+    } catch (Exception e) {
+        System.out.println("end Test case: testCase03");
+    }
+    }
+
+   
+    public  boolean testCase04(){
+        System.out.println("Start Test case: testCase04");
+         
+ 
+        WebElement submissionTab = driver.findElement(By.id("submissions_tab"));
+         submissionTab.click();
+ 
+        try {
+             Thread.sleep(10000); // Wait for the page to load
+         } catch (InterruptedException e) {
+             System.out.println("Sleep interrupted: " + e.getMessage());
+         }
+        System.out.println("end Test case: testCase04");
+        System.out.println("end Test case: testCase04");
+      
+        String res= driver.findElement(By.xpath("//a[text()='Register or Sign In']")).getText();
+        if(res.contains("Register or Sign In")){
+         return true;
+        }
+           return false;
+    }
+
+
+}
